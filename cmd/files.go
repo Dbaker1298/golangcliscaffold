8e11719661e5fd525b690041222296e8bc3c8353
David Baker <dpbaker1298@gmail.com>
2023-11-04 16:59:26 -0400

    illustrate logrus with Viper AllSettings log

--- cmd/files.go
+++ cmd/files.go
@@ -5,9 +5,10 @@ Copyright © 2023 David Baker <dpbaker1298@gmail.com>
 package cmd
 
 import (
-	"fmt"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // filesCmd represents the files command
@@ -16,20 +17,14 @@ var filesCmd = &cobra.Command{
 	Short: "Show the largest files in the given path.",
 	Long: `Quickly scan a directory and find large files.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("files called")
+		for key, value := range viper.GetViper().AllSettings() {
+			log.WithFields(log.Fields{
+				key: value,
+			}).Info("Command Flag")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(filesCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// filesCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// filesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
